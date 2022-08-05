4af5888efb29f8d4a90ca25dbf89d36274473cab
Avinash Patil <avinashspatil12@gmail.com>
2022-08-05 22:45:47 +0530

    branch B commits

--- src/constructor/Parent.java
+++ src/constructor/Parent.java
@@ -3,7 +3,8 @@ package constructor;
 public class Parent {
 	
 	protected void m1() {
-		System.out.println("parent class method");
+		System.out.println("parent class method8910");
+		System.out.println("new line branch B");
 	}
 
 }
